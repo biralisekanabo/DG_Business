0e073987118b47d89c741122507bfee21beaead3
Blessing print <blessingprint203@gmail.com>
2026-06-03 11:35:12 +0200

    Premier push

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>DG Business</title>
+      <meta name="description" content="Application mobile et desktop de gestion" />
+    </>
+  );
+}
